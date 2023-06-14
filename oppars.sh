905c73c5b5246aae33321f8f44868fe8e78db0af
wolender <wolender@griddynamics.com>
2023-06-14 20:07:01 +0200

    adding debug option

--- oppars.sh
+++ oppars.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #sequence of numbers task
 
-while getopts o:n:d flag
+print_debug_info() {
+  echo "User: $USER"
+  echo "Script: $0"
+  echo "Operation: $operator"
+  echo "Numbers: ${numbers[*]}"
+}
+debug=0
+while getopts "o:n:d" flag;
 do
     case "${flag}" in
-        o) operator=${OPTARG};;
-        n) numbers=${OPTARG[@]};;
-        d) debug=1;;
+        o) operator=${OPTARG} ;;
+        n) numbers=${OPTARG[@]} ;;
+        d) debug=1 ;;
     esac
 done
+
+if [ $debug -eq 1 ]; then
+    print_debug_info
+fi  
     
