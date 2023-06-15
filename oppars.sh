40fc23c0695d3028fa56a8d6a4a50f884ad0d845
wolender <wolender@griddynamics.com>
2023-06-16 00:17:34 +0200

    task two finished

--- oppars.sh
+++ oppars.sh
@@ -5,19 +5,62 @@ print_debug_info() {
   echo "User: $USER"
   echo "Script: $0"
   echo "Operation: $operator"
-  echo "Numbers: ${numbers[*]}"
+  echo "Numbers: ${#numbers[@]}"
 }
 debug=0
 while getopts "o:n:d" flag;
 do
     case "${flag}" in
-        o) operator=${OPTARG} ;;
-        n) numbers=${OPTARG[@]} ;;
-        d) debug=1 ;;
+        o) 
+            operator=${OPTARG} 
+            
+            ;;
+        d) 
+            debug=1 
+            
+            ;;
+        n)
+            numbers=()
+            shift $((OPTIND - 2))
+            while [[ $# -gt 0 ]] && [[ $1 != -* ]]; do
+                numbers+=("$1")
+                shift
+            done
+            ;;
+        
     esac
 done
 
 if [ $debug -eq 1 ]; then
     print_debug_info
 fi  
-    
+
+result=${numbers[0]}
+case $operator in
+  -)
+    for ((i = 1; i < ${#numbers[@]}; i++)); do
+      result=$((result - numbers[i]))
+    done
+    ;;
+  +)
+    for ((i = 1; i < ${#numbers[@]}; i++)); do
+      result=$((result + numbers[i]))
+    done
+    ;;
+  \*)
+    for ((i = 1; i < ${#numbers[@]}; i++)); do
+      result=$((result * numbers[i]))
+    done
+    ;;
+  %)
+    for ((i = 1; i < ${#numbers[@]}; i++)); do
+      result=$((result / numbers[i]))
+    done
+    ;;
+  *)
+    echo "Invalid operation: $operation" >&2
+    exit 1
+    ;;
+esac
+
+echo "The ressult is : $result"
